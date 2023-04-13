2b7afae9c8ff4c2e61cd4d3c585e32b69709bae6
Bonsly24 <47396212+Bonsly24@users.noreply.github.com>
2023-04-12 20:31:24 -0500

    Stuff

--- lib/philomena/users/user.ex
+++ lib/philomena/users/user.ex
@@ -505,11 +505,11 @@ defmodule Philomena.Users.User do
     provisioning_uri = %URI{
       scheme: "otpauth",
       host: "totp",
-      path: "/YourBooruName:" <> user.email,
+      path: "/Misakabooru:" <> user.email,
       query:
         URI.encode_query(%{
           secret: secret,
-          issuer: "YourBooruName"
+          issuer: "Misakabooru"
         })
     }
 
