bed74f46243bfc328dd0f69017e968e5782253d5
Bonsly24 <47396212+Bonsly24@users.noreply.github.com>
2023-04-25 13:47:59 -0500

    Rebranding

--- lib/philomena_web/views/api/json/oembed_view.ex
+++ lib/philomena_web/views/api/json/oembed_view.ex
@@ -11,16 +11,16 @@ defmodule PhilomenaWeb.Api.Json.OembedView do
     %{
       version: "1.0",
       type: "photo",
-      title: "##{image.id} - #{tag_list(image)} - Derpibooru",
+      title: "##{image.id} - #{tag_list(image)} - Misakabooru",
       author_url: image.source_url || "",
       author_name: artist_tags(image.tags),
-      provider_name: "Derpibooru",
+      provider_name: "Misakabooru",
       provider_url: PhilomenaWeb.Endpoint.url(),
       cache_age: 7200,
-      derpibooru_id: image.id,
-      derpibooru_score: image.score,
-      derpibooru_comments: image.comments_count,
-      derpibooru_tags: Enum.map(image.tags, & &1.name)
+      misakabooru_id: image.id,
+      misakabooru_score: image.score,
+      misakabooru_comments: image.comments_count,
+      misakabooru_tags: Enum.map(image.tags, & &1.name)
     }
   end
 
